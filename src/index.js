fd8c4a770c9174a1686f03e33cbc704265fa6b97
T-queen <142670502+T-queen@users.noreply.github.com>
2023-10-02 23:18:13 +0100

    Added the relevant icon from the Api

--- src/index.js
+++ src/index.js
@@ -29,16 +29,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "2b1ee82be7aebff078121568f54f3a4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=2b1ee82be7aebff078121568f54f3a4c&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2b1ee82be7aebff078121568f54f3a4c&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
